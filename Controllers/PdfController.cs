b34fdf8d3a0a5e6010d7280fa9e3813b07391014
AkhtarGul <akhtar0316169@gmail.com>
2023-12-27 14:58:50 +0500

    fixed

---
+++ Controllers/PdfController.cs
@@ -0,0 +1,51 @@
+﻿using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+
+namespace pdfs_extract.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+        private readonly IHostEnvironment _hostEnvironment;
+        private readonly PdfExtractorService _pdfExtractorService;
+
+        public PdfController(IHostEnvironment hostEnvironment, PdfExtractorService pdfExtractorService)
+        {
+            _hostEnvironment = hostEnvironment;
+            _pdfExtractorService = pdfExtractorService;
+        }
+
+        [HttpGet("extractText")]
+        public IActionResult ExtractTextFromPdf()
+        {
+            try
+            {
+                var wwwRootPath = _hostEnvironment.ContentRootPath; // Assuming wwwroot is at the project's root level
+                var filePath = Path.Combine(wwwRootPath, "www", "Transcript.pdf");
+
+                if (!System.IO.File.Exists(filePath))
+                {
+                    return NotFound(new { Error = "File not found" });
+                }
+
+                var pdfBytes = System.IO.File.ReadAllBytes(filePath);
+                var extractedText = _pdfExtractorService.ExtractStudentInfoFromPdf(pdfBytes);
+
+                return Ok(new { Text = extractedText });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+    }
+}
+
---
+++ Services/PdfExtractorService.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+public class PdfExtractorService
+{
+    public string ExtractStudentInfoFromPdf(byte[] pdfBytes)
+    {
+        using (var pdfReader = new PdfReader(new MemoryStream(pdfBytes)))
+        {
+            using (var pdfDocument = new PdfDocument(pdfReader))
+            {
+                var text = new StringWriter();
+                var strategy = new LocationTextExtractionStrategy();
+
+                for (var page = 1; page <= pdfDocument.GetNumberOfPages(); page++)
+                {
+                    var currentText = PdfTextExtractor.GetTextFromPage(pdfDocument.GetPage(page), strategy);
+                    text.Write(currentText);
+                }
+
+                return ExtractStudentInfo(text.ToString());
+            }
+        }
+    }
+
+    private string ExtractStudentInfo(string pdfText)
+    {
+        // Define a regular expression pattern to match the student's name and registration number
+        string pattern = @"Student's Name: (?<Name>.+?) Registration No: (?<RegistrationNo>\d+)";
+
+        // Use Regex.Match to find the match
+        Match match = Regex.Match(pdfText, pattern);
+
+        // Check if the match is successful
+        if (match.Success)
+        {
+            // Access the captured groups using the group names
+            string studentName = match.Groups["Name"].Value;
+            string registrationNo = match.Groups["RegistrationNo"].Value;
+
+            // Construct a string with the extracted information
+            return $"Student's Name: {studentName}\nRegistration No: {registrationNo}";
+        }
+        else
+        {
+            return "Match not found";
+        }
+    }
+}
